fc5183792a402090f0b2c900ac5d728d6d6c10c4
dexter11235813 <abgairola@gmail.com>
2016-10-15 17:04:32 -0700

    upd

--- Text-Mining/Twitter-wordcloud.R
+++ Text-Mining/Twitter-wordcloud.R
@@ -1,8 +1,10 @@
+setwd("/home/abhijit331/GeneralR/Text-Mining")
 library(tm)
 library(twitteR)
 library(dplyr)
 library(wordcloud)
 library(RColorBrewer)
+library(RWeka)
 
 consumer_key = "PKIqzBYSv4TRN4SnR8MXnW1kQ"
 consumer_secret = "ZRWS4RXUDDe4o3FWArxUofthP7zjnUa3lv7gzwRSYQMqtWkJi9"
@@ -11,12 +13,12 @@ access_token_secret = "GHjsDvBE0qLAYA38sEC2FJU8mQfyVwOnxJn5OeKTSxdWT"
 
 setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_token_secret)
 
-target.tweets = searchTwitter("BJP",n = 5000,lang = "en")
+target.tweets = searchTwitter("Modi",n = 1000,lang = "en")
 ######
 
 target.tweets.text = sapply(target.tweets,function(x) x$getText())  
 target.tweets.text = iconv(target.tweets.text,to = "utf-8", sub = "")
-
+target.tweets.text = gsub("^http(.*?)\\s","",target.tweets.text)
 target.corpus = VCorpus(VectorSource(target.tweets.text))
 
 target.corpus =  tm_map(target.corpus,removePunctuation)
@@ -29,7 +31,12 @@ target.corpus = tm_map(target.corpus,content_transformer(tolower),lazy = T)
 # target.corpus
 #target.corpus = VCorpus(VectorSource(target.corpus))
 
-target.tdm = TermDocumentMatrix(target.corpus,control =  list(weighting = weightTfIdf))
+tokenizer = function(x)
+{
+  NGramTokenizer(x,Weka_control(min = 2 , max = 2))
+}
+
+target.tdm = TermDocumentMatrix(target.corpus,control =  list(tokenize = tokenizer,weighting = weightTfIdf))
 
 
 
